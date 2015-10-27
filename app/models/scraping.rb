3276d40dd6bd7bce2cbf3568438d89666de0e882
TaniguchiEriko <taniguchieriko@TaniguchiEriko-no-MacBook-Air.local>
2015-10-27 14:45:20 +0900

    did scraping

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,11 +1,20 @@
 class Scraping
   def self.movie_urls
-    links = []
     agent = Mechanize.new
-    current_page = agent.get("http://eiga.com/now/")
-    elements = current_page.search('.m_unit h3 a')
-    elements.each do |ele|
-      links << ele.get_attribute('href')
+    links = []
+
+    next_url = "/now/"
+
+    while true do
+      current_page = agent.get("http://eiga.com" + next_url)
+      elements = current_page.search('.m_unit h3 a')
+      elements.each do |ele|
+        links << ele.get_attribute('href')
+      end
+
+      next_link = current_page.at('.next_page')
+      next_url = next_link.get_attribute('href')
+      break unless next_url
     end
 
     links.each do |link|
@@ -17,9 +26,9 @@ class Scraping
     agent = Mechanize.new
     page = agent.get(link)
     title = page.at('.moveInfoBox h1').inner_text
-    image_url = page.at('.pictBox img')[:src] if page.at('.pictBox img')
+    image_url = page.at('.pictBox img')[:src]
 
-    product = Product.new(title: title, image_url: image_url)
+    product = Product.where(title: title, image_url: image_url).first_or_initialize
     product.save
   end
 end
